46628e27a677223c08990f30172aaf1e5d436836
Dmytro Morozov <dmytro.morozov@check24.de>
2015-08-25 14:36:47 +0200

    Structure project layout

---
+++ sources/EnterpriseLibrary.SemanticLogging.Mongo/MongoSink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+
+namespace EnterpriseLibrary.SemanticLogging.Mongo
+{
+    public class MongoSink : IObserver<EventEntry>, IDisposable
+    {
+        public void OnNext(EventEntry value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
